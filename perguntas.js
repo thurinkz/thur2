71e3aa4b2ca5070adaea01d46ca20838d9f90bb7
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-12-05 17:20:17 -0300

    thur

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Uma variável é um espaço na memória usado para armazenar dados que podem ser alterados durante a execução do programa.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa a sigla HTML?',
+    'HTML significa HyperText Markup Language, que é uma linguagem de marcação usada para criar páginas da web.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a tradução de "book" em português?',
+    'A tradução de "book" em português é "livro".'
+)
